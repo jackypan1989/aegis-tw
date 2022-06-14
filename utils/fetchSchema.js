351f5a63c3bd5b1f24f6d310123aa543126477f8
Guan Yu Pan <jackypan1989@gmail.com>
2022-06-14 16:25:12 +0800

    chore: upgrade package

---
+++ utils/fetchSchema.js
@@ -0,0 +1,39 @@
+require("dotenv").config()
+
+const fs = require("fs")
+const path = require("path")
+const { fetch } = require("cross-undici-fetch")
+const {
+  buildClientSchema,
+  getIntrospectionQuery,
+  printSchema,
+} = require("graphql")
+
+const fetchGraphQLSchema = (url, options = {}) => {
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      apiKey: process.env.SUPABASE_ANON_KEY,
+    },
+    body: JSON.stringify({
+      query: getIntrospectionQuery(),
+    }),
+  })
+    .then((res) => res.json())
+    .then((schemaJSON) => {
+      if (options.readable) {
+        return printSchema(buildClientSchema(schemaJSON.data))
+      }
+
+      return JSON.stringify(schemaJSON, null, 2)
+    })
+}
+const filePath = path.join(__dirname, "../graphql/schema/", "schema.graphql")
+
+fetchGraphQLSchema(`${process.env.SUPABASE_URL}/graphql/v1`, {
+  readable: true,
+}).then((schema) => {
+  fs.writeFileSync(filePath, schema, "utf-8");
+});
