8b5e6dd49a4f3081632f34f43b7ee2d6c9f591b2
Guan Yu Pan <jackypan1989@gmail.com>
2022-07-14 05:35:17 +0800

    chore: update merged sql

--- merged.sql
+++ merged.sql
@@ -208,86 +208,11 @@ GRANT EXECUTE ON FUNCTION public.handle_new_user() TO authenticated;
 
 GRANT EXECUTE ON FUNCTION public.handle_new_user() TO service_role;
 
-GRANT EXECUTE ON FUNCTION public.handle_new_user() TO PUBLIC;-- FUNCTION: public.update_ranking_score()
-
--- DROP FUNCTION IF EXISTS public.update_ranking_score();
-
-CREATE OR REPLACE FUNCTION public.update_ranking_score()
-    RETURNS trigger
-    LANGUAGE 'plpgsql'
-    COST 100
-    VOLATILE NOT LEAKPROOF SECURITY DEFINER
-AS $BODY$
-BEGIN
-
-WITH r AS (
-SELECT
-	coalesce(votes.post_id, posts.id) AS post_id,
-	coalesce(sum(1), 0) AS vote_count,
-    round(
-        coalesce(
-            power(coalesce(sum(1), 0), 0.8) / power((DATE_PART('day', now() - posts.created_at) * 24 + DATE_PART('hour', now() - posts.created_at) + 2), 1.8) * 1000000 / (posts.view_count + 1)
-            , -2147483648
-        )::numeric 
-    , 0) AS ranking_score
-FROM
-	votes
-	RIGHT JOIN posts ON votes.post_id = posts.id
-GROUP BY
-	posts.id,
-	votes.post_id
-)
-
-UPDATE
-	public.posts
-SET
-	vote_count = r.vote_count,
-    ranking_score = r.ranking_score
-FROM
-	r
-WHERE
-	r.post_id = public.posts.id;
-
-RETURN new;
-END;
-$BODY$;
-
-ALTER FUNCTION public.update_ranking_score()
-    OWNER TO postgres;
-
-GRANT EXECUTE ON FUNCTION public.update_ranking_score() TO authenticated;
-
-GRANT EXECUTE ON FUNCTION public.update_ranking_score() TO postgres;
-
-GRANT EXECUTE ON FUNCTION public.update_ranking_score() TO PUBLIC;
-
-GRANT EXECUTE ON FUNCTION public.update_ranking_score() TO anon;
-
-GRANT EXECUTE ON FUNCTION public.update_ranking_score() TO service_role;
-
--- Trigger: on_auth_user_created
+GRANT EXECUTE ON FUNCTION public.handle_new_user() TO PUBLIC;-- Trigger: on_auth_user_created
 
 -- DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
 
 CREATE TRIGGER on_auth_user_created 
     AFTER INSERT ON auth.users 
     FOR EACH ROW 
-    EXECUTE FUNCTION public.handle_new_user();-- Trigger: on_vote_created
-
--- DROP TRIGGER IF EXISTS on_vote_created ON public.vote;
-
-CREATE TRIGGER on_vote_created
-    AFTER INSERT
-    ON public.votes
-    FOR EACH ROW
-    EXECUTE FUNCTION public.update_ranking_score();
-
--- Trigger: on_vote_deleted
-
--- DROP TRIGGER IF EXISTS on_vote_deleted ON public.vote;
-
-CREATE TRIGGER on_vote_deleted
-    AFTER DELETE
-    ON public.votes
-    FOR EACH ROW
-    EXECUTE FUNCTION public.update_ranking_score();
+    EXECUTE FUNCTION public.handle_new_user();
