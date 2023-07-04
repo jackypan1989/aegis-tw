5c8a2768ad1238a5a48cd82a8e53a12cb13513b7
Guan Yu Pan <jackypan1989@gmail.com>
2023-07-04 11:50:52 +0800

    feat: refactor landing page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,47 +1,41 @@
 import { Box, ChakraProvider, Heading, Link, ListItem, OrderedList, Text, UnorderedList } from "@chakra-ui/react"
 import { MDXProvider } from "@mdx-js/react"
-import { createBrowserSupabaseClient, Session } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { Session } from '@supabase/auth-helpers-nextjs'
 import { MDXComponents } from 'mdx/types'
 import { AppProps } from 'next/app'
 import Head from "next/head"
-import { useRouter } from "next/router"
 import Script from "next/script"
-import { useState } from "react"
-import Banner from "../components/banner"
 import Footer from '../components/footer'
 import Navbar from '../components/navbar'
 import '../styles/globals.css'
-import { CustomApolloProvider } from '../utils/customApolloProvider'
+import { Image } from "@chakra-ui/react"
 
 const component: MDXComponents = {
-  h1: (props) => <Heading as='h1' size='2xl' mb='4' {...props} />,
-  h2: (props) => <Heading as='h2' size='xl' mb='4' {...props} />,
-  h3: (props) => <Heading as='h3' size='lg' mb='4' {...props} />,
-  h4: (props) => <Heading as='h4' size='md' mb='4' {...props} />,
-  h5: (props) => <Heading as='h5' size='sm' mb='4' {...props} />,
-  h6: (props) => <Heading as='h6' size='xs' mb='4' {...props} />,
+  h1: (props) => <Heading as='h1' size='xl' mb='4' {...props} />,
+  h2: (props) => <Heading as='h2' size='lg' mb='4' {...props} />,
+  h3: (props) => <Heading as='h3' size='md' mb='4' {...props} />,
+  h4: (props) => <Heading as='h4' size='sm' mb='4' {...props} />,
+  h5: (props) => <Heading as='h5' size='xs' mb='4' {...props} />,
+  h6: (props) => <Heading as='h6' size='2xs' mb='4' {...props} />,
   p: (props) => <Text mb='4' lineHeight={1.6} {...props} />,
-  a: (props) => <Link {...props} />,
+  a: (props) => <a {...props} />,
   ol: (props) => <OrderedList mb='4' {...props} />,
   ul: (props) => <UnorderedList mb='4' {...props} />,
   li: (props) => <ListItem mb='1' {...props} />,
 }
 
-function MyApp({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
-  const router = useRouter()
-  const [supabaseClient] = useState(() =>
-    createBrowserSupabaseClient<any>()
-  )
+const title = 'Aegis Venture - Startup Advisory Service for SaaS, AI, and Social Startups'
+const description = 'Aegis Venture is a leading startup advisory firm in Taiwan, offering comprehensive support and guidance to SaaS, AI, and Social startups. Our experienced team of founders, Jacky Pan and Oscar Chien, provide expert tech, product, operations, legal, and fundraising assistance, helping startups secure funding, connect with partners, and achieve their entrepreneurial goals. Contact us today to turn your startup dreams into reality.'
 
+function MyApp({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
   return (
     <>
       <Head>
-        <title>Aegis Taiwan | 台灣人軟體新創社群</title>
-        <meta name="description"  content="Aegis Taiwan 是以軟體業為出發，擁有台灣人認同，串連海內外台灣人的跨境新創社群。" />
+        <title>{title}</title>
+        <meta name="description"  content={description} />
         <meta property="og:type" content="article" />
-        <meta property="og:title" content="Aegis Taiwan | 台灣人軟體新創社群" />
-        <meta property="og:description" content="Aegis Taiwan 是以軟體業為出發，擁有台灣人認同，串連海內外台灣人的跨境新創社群。" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
@@ -58,23 +52,24 @@ function MyApp({ Component, pageProps }: AppProps<{ initialSession: Session }>) 
           gtag('config', 'G-REPJEXSGK2');
         `}
       </Script>
-      <SessionContextProvider 
-        supabaseClient={supabaseClient}
-        initialSession={pageProps.initialSession}
-      >
-        <CustomApolloProvider>
-          <MDXProvider components={component}>
-            <ChakraProvider>
-              <Navbar />
-              {/* {router.pathname === '/' && <Banner />} */}
-              <Box m={{ lg: '0 auto'}} width={{ lg: '800px' }} minH={{ base: 'calc(100vh - 96px)', lg: 'calc(100vh - 128px)' }}>
-                <Component {...pageProps} />
-              </Box>
-              <Footer />
-            </ChakraProvider>
-          </MDXProvider>
-        </CustomApolloProvider>
-      </SessionContextProvider>
+      <MDXProvider components={component}>
+        <ChakraProvider>
+          <Box 
+            height='200px'
+            backgroundImage='https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=4800'
+            backgroundSize='cover'
+          >
+          </Box>
+          <Box 
+            m={{ lg: '0 auto' }} 
+            p={30}
+            width={{ lg: '800px' }} 
+            minH={{ base: 'calc(100vh - 96px)', lg: 'calc(100vh - 128px)' }}
+          >
+            <Component {...pageProps} />
+          </Box>
+        </ChakraProvider>
+      </MDXProvider>
     </>
   )
 }
